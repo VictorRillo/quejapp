77cad0f49d7d3aa0b3bb4b2937f2e681f924ff42
Victor Rillo Martinez <victorrillo96@gmail.com>
2024-03-04 23:43:21 +0100

    Header translations added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import "./Header.scss";
 import logo from "../../assets/images/logo.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="header">
       <img src={logo} alt="Logo" className="logo" />
       <div className="menu">
         <a className="menu-item" href="#">
-          Listado
+          {t("complaint_list")}
         </a>
         <a className="menu-item" href="#">
-          Añadir queja
+          {t("add_complaint")}
         </a>
       </div>
-      <FontAwesomeIcon icon={faUser} className="login-icon" size="2x"/>
+      <button className="login-icon" aria-label={t('login')}>
+        <FontAwesomeIcon icon={faUser} size="2x" />
+      </button>
     </div>
   );
 };
